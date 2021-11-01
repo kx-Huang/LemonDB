7befae34af678492e12e73fe44c47d606e73b9fb
李向杰518370910216 <superswift@sjtu.edu.cn>
2021-11-02 02:44:03 +0800

    上传文件至 'src/query/data'

--- src/query/data/AddQuery.cpp
+++ src/query/data/AddQuery.cpp
@@ -2,8 +2,52 @@
 #include <iostream>
 
 #include "../../db/Database.h"
-
+#include <pthread.h>
+#include <semaphore.h>
+#include <algorithm>
+#include "../../multithreads/MultiThread.h"
+/**********************************************/
+/*Define Global Varaibles*/
 constexpr const char *AddQuery::qname;
+static size_t counter;
+static unsigned int current_thread;
+static unsigned int total_thread;
+static unsigned int subtable_num;
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+static Table* copy_table;
+static ComplexQuery* copy_this; 
+static std::vector<std::string> *copy_operand;
+static std::pair<std::string, bool> result;
+/**********************************************/
+
+void* Sub_AddQuery(void*){  
+    pthread_mutex_lock(&mutex);
+    int id = (int)current_thread;
+    current_thread++;
+    pthread_mutex_unlock(&mutex);
+    
+    auto head = copy_table->begin() + (id*(int)subtable_num);
+    auto tail = id == (int)total_thread - 1 ?
+                                      copy_table->end():head + (int)subtable_num;
+
+    if (result.second){
+      int sub_counter = 0;
+       for (auto item = head; item != tail; item++) {
+        if (copy_this->evalCondition(*item)) {
+          int sum = 0;
+          sub_counter ++;
+          for (auto key = copy_operand->begin(); key != copy_operand->end()-1; key++) {
+            sum += (*item)[*key]; 
+          }
+          (*item)[*(copy_operand->end()-1)] = sum;
+        }
+      }
+        pthread_mutex_lock(&mutex);
+        counter = counter + (size_t) sub_counter;
+        pthread_mutex_unlock(&mutex);
+    }
+     return NULL;
+}
 
 QueryResult::Ptr AddQuery::execute() {
   using namespace std;
@@ -15,10 +59,14 @@ QueryResult::Ptr AddQuery::execute() {
   // start of try
   try {
     auto &table = db[this->targetTable];
-    auto result = initCondition(table);
-    size_t counter = 0;
-    if (result.second) {
-      for (auto it = table.begin(); it != table.end(); ++it) {
+    result = initCondition(table);
+    counter = 0;
+    total_thread = get_ThreadNum();
+  //  return make_unique<RecordCountResult>(total_thread*100);
+    copy_operand = &this->operands;
+    if (total_thread < 2 || table.size()< 16 ){
+      if (result.second) {
+       for (auto it = table.begin(); it != table.end(); ++it) {
         if (this->evalCondition(*it)) {
           int sum = 0;
           for (auto key = this->operands.begin(); key != this->operands.end()-1; key++) {
@@ -28,8 +76,28 @@ QueryResult::Ptr AddQuery::execute() {
           counter++;
         }
       }
+      return make_unique<RecordCountResult>(counter);
     }
-    return make_unique<RecordCountResult>(counter);
+    }
+    else{
+      copy_table = &table;
+      copy_this = this;
+      subtable_num =(unsigned int) (table.size())/total_thread;
+      pthread_t *store=new pthread_t[total_thread - 1];
+      current_thread = 0;
+      for(int i = 0;i < (int)total_thread - 1; i++){
+        pthread_create(store+i, NULL, Sub_AddQuery, &i);
+      }
+      Sub_AddQuery(NULL);
+      for(int i = 0; i < (int)total_thread - 1; i++){
+                pthread_join(*(store+i), NULL);
+      }       
+      delete [] store;
+      return make_unique<RecordCountResult>(counter);
+    }
+     return make_unique<ErrorMsgResult>(
+        qname, this->targetTable.c_str(),
+        "Fuck You!");
   } catch (const TableNameNotFound &e) {
     return make_unique<ErrorMsgResult>(qname, this->targetTable,
                                        "No such table."s);
--- src/query/data/SubQuery.cpp
+++ src/query/data/SubQuery.cpp
@@ -2,8 +2,56 @@
 #include <iostream>
 
 #include "../../db/Database.h"
+#include <pthread.h>
+#include <semaphore.h>
+#include <algorithm>
+#include "../../multithreads/MultiThread.h"
 
+/**********************************************/
+/*Define Global Varaibles*/
+
+static size_t counter;
+static unsigned int current_thread;
+static unsigned int total_thread;
+static unsigned int subtable_num;
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+static Table* copy_table;
+static std::pair<std::string, bool> result;
+static ComplexQuery* copy_this; 
+static std::vector<std::string> *copy_operand;
 constexpr const char *SubQuery::qname;
+/**********************************************/
+
+void* Sub_SubQuery(void*){  
+    pthread_mutex_lock(&mutex);
+    int id = (int)current_thread;
+    current_thread++;
+    pthread_mutex_unlock(&mutex);
+    
+    auto head = copy_table->begin() + (id*(int)subtable_num);
+    auto tail = id == (int)total_thread - 1 ?
+                                      copy_table->end():head + (int)subtable_num;
+    if (result.second){
+      int sub_counter = 0;
+       for (auto it = head; it != tail; it++) {
+        if (copy_this->evalCondition(*it)) {
+          sub_counter++;
+          int ans = (*it)[*(copy_operand->begin())];
+          for (auto key = copy_operand->begin()+1; key != copy_operand->end()-1; key++) {
+            ans -= (*it)[*key]; 
+          }
+          (*it)[*(copy_operand->end()-1)] = ans;     
+        }
+    }
+    pthread_mutex_lock(&mutex);
+    counter = counter + (size_t) sub_counter;
+    pthread_mutex_unlock(&mutex);
+    }
+     return NULL;
+
+}
+
+
 
 QueryResult::Ptr SubQuery::execute() {
   using namespace std;
@@ -15,8 +63,11 @@ QueryResult::Ptr SubQuery::execute() {
   // start of try
   try {
     auto &table = db[this->targetTable];
-    auto result = initCondition(table);
-    size_t counter = 0;
+    result = initCondition(table);
+    total_thread = get_ThreadNum();
+    counter = 0;
+    copy_operand = &this->operands;
+    if (table.size()<16 || total_thread < 2 ){
     if (result.second) {
       for (auto it = table.begin(); it != table.end(); ++it) {
         if (this->evalCondition(*it)) {
@@ -28,8 +79,28 @@ QueryResult::Ptr SubQuery::execute() {
           counter++;
         }
       }
+      return make_unique<RecordCountResult>(counter);
     }
-    return make_unique<RecordCountResult>(counter);
+    }
+    else{
+      copy_table = &table;
+      copy_this = this;
+      subtable_num =(unsigned int) (table.size())/total_thread;
+      pthread_t *store=new pthread_t[total_thread - 1];
+      current_thread = 0;
+      for(int i = 0;i < (int)total_thread - 1; i++){
+        pthread_create(store+i, NULL, Sub_SubQuery, &i);
+      }
+      Sub_SubQuery(NULL);
+      for(int i = 0; i < (int)total_thread - 1; i++){
+                pthread_join(*(store+i), NULL);
+      }       
+      delete [] store;
+      return make_unique<RecordCountResult>(counter);
+    }
+    return make_unique<ErrorMsgResult>(
+        qname, this->targetTable.c_str(),
+        "Holy Shit!");
   } catch (const TableNameNotFound &e) {
     return make_unique<ErrorMsgResult>(qname, this->targetTable,
                                        "No such table."s);
