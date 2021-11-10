7f2725b38d92a95c2c19a063a5a5ab49e625c651
李向杰518370910216 <superswift@sjtu.edu.cn>
2021-11-10 21:01:31 +0800

    上传文件至 'src/query/data'

--- src/query/data/MinQuery.cpp
+++ src/query/data/MinQuery.cpp
@@ -1,37 +1,84 @@
 #include "MinQuery.h"
 #include "../../db/Database.h"
-
+#include "../../multithreads/MultiThread.hpp"
+/**********************************************/
+/*Define Global Varaibles*/
 constexpr const char *MinQuery::qname;
+static unsigned int total_thread;
+static unsigned int subtable_num;
+
+static Table *copy_table;
+static ComplexQuery *copy_this;
+static std::vector<std::string> *copy_operand;
+static std::pair<std::string, bool> result;
+static int *int_arr;
+static bool found;
+/**********************************************/
+
+void Sub_min(int id){
+  auto head = copy_table->begin() + (id * (int)subtable_num);
+  auto tail = id == (int)total_thread - 1 ? copy_table->end()
+                                          : head + (int)subtable_num;
+  if (result.second) {
+    for (auto row = head; row != tail; row++){
+      if (copy_this->evalCondition(*row)) {
+          found = true;
+          for (size_t i(0); i < copy_operand->size(); i++) {
+            if (int_arr[i] > (*row)[(*copy_operand)[i]]) {
+              int_arr[i] = (*row)[(*copy_operand)[i]];
+            }
+          }
+        }
+    }
+  }
+}
+
 
 QueryResult::Ptr MinQuery::execute() {
   using namespace std;
-  auto size = this->operands.size();
-  if (size == 0)
+  if (this->operands.size() == 0)
     return make_unique<ErrorMsgResult>(
         qname, this->targetTable.c_str(),
-        "Invalid number of operands (? operands)."_f % size);
+        "Invalid number of operands (? operands)."_f % operands.size());
   Database &db = Database::getInstance();
   // start of try
   try {
-    bool found = false;
+      found = false;
     auto &table = db[this->targetTable];
-    auto result = initCondition(table);
-    int *int_arr = new int[(this->operands).size()];
-    for (size_t i = 0; i < size; i++)
+    result = initCondition(table);
+    int_arr = new int[(this->operands).size()];
+    total_thread = (unsigned int) worker.Thread_count();
+    for (size_t i(0); i < this->operands.size(); i++)
       int_arr[i] = INT32_MAX;
+    if (total_thread < 2 || table.size() < 16){
     if (result.second) {
       for (auto row = table.begin(); row != table.end(); ++row) {
         if (this->evalCondition(*row)) {
           found = true;
-          for (size_t i(0); i < size; i++) {
-            if (int_arr[i] > (*row)[this->operands[i]])
+          for (size_t i(0); i < this->operands.size(); i++) {
+            if (int_arr[i] > (*row)[this->operands[i]]) {
               int_arr[i] = (*row)[this->operands[i]];
+            }
           }
         }
       }
     }
+    }
+    else{
+      copy_table = &table;
+      copy_this = this;
+      subtable_num = (unsigned int)(table.size()) / total_thread;
+       std::vector<std::future<void>> futures((unsigned long)total_thread);
+      for(int i = 0; i<(int)total_thread - 1; i++){
+        futures[(unsigned long)i] = worker.Submit(Sub_min, i);
+      }
+      Sub_min((int)total_thread - 1);
+      for (unsigned long i = 0; i<(unsigned long)total_thread - 1;i++){
+        futures[i].get();
+      }
+    }
     if (found)
-      return make_unique<SuccessMsgResult>(int_arr, size);
+      return make_unique<SuccessMsgResult>(int_arr, this->operands.size());
     else
       return make_unique<NullQueryResult>();
   } catch (const TableNameNotFound &e) {
