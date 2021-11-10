7d7fb369c4618a8153a6434873475f3b5f4488db
李向杰518370910216 <superswift@sjtu.edu.cn>
2021-11-10 21:01:18 +0800

    上传文件至 'src/query/data'

--- src/query/data/SelectQuery.cpp
+++ src/query/data/SelectQuery.cpp
@@ -1,9 +1,10 @@
 #include "SelectQuery.h"
 
 #include "../../db/Database.h"
-
+#include "../../multithreads/MultiThread.hpp"
 #include <queue>
-
+/**********************************************/
+/*Define Global Varaibles*/
 struct prioritize {
   bool operator()(Table::Iterator &a, Table::Iterator &b) {
     const std::string &key1(a->key()), &key2(b->key());
@@ -22,6 +23,29 @@ struct prioritize {
 };
 
 constexpr const char *SelectQuery::qname;
+static unsigned int total_thread;
+static unsigned int subtable_num;
+static Table *copy_table;
+static ComplexQuery *copy_this;
+static std::pair<std::string, bool> result;
+using namespace std;
+static priority_queue<Table::Iterator, vector<Table::Iterator>, prioritize>
+        priorityQueue;
+/**********************************************/
+void Sub_Select(int id){
+  auto head = copy_table->begin() + (id * (int)subtable_num);
+  auto tail = id == (int)total_thread - 1 ? copy_table->end()
+                                          : head + (int)subtable_num;
+  if (result.second) {
+    for (auto item = head; item != tail; item++) {
+      if (copy_this->evalCondition(*item) == true)
+          priorityQueue.push(item);
+    }
+  }
+  return;
+}
+
+
 
 QueryResult::Ptr SelectQuery::execute() {
   using namespace std;
@@ -29,16 +53,31 @@ QueryResult::Ptr SelectQuery::execute() {
   // start of try
   try {
     auto &table = db[this->targetTable];
-    auto result = initCondition(table);
-    priority_queue<Table::Iterator, vector<Table::Iterator>, prioritize>
-        priorityQueue;
+    result = initCondition(table);
+    total_thread = (unsigned int) worker.Thread_count();
+   
     std::ostringstream oss;
+
     if (result.second) {
       for (auto it(table.begin()); it != table.end(); ++it) {
         if (this->evalCondition(*it) == true)
           priorityQueue.push(it);
       }
-      if (priorityQueue.size() == 0)
+    }
+    else{
+      copy_table = &table;
+      copy_this = this;
+      subtable_num = (unsigned int)(table.size()) / total_thread;
+      std::vector<std::future<void>> futures((unsigned long)total_thread);
+      for(int i = 0; i<(int)total_thread - 1; i++){
+        futures[(unsigned long)i] = worker.Submit(Sub_Select, i);
+      }
+      Sub_Select((int)total_thread - 1);
+      for (unsigned long i = 0; i<(unsigned long)total_thread - 1;i++){
+        futures[i].get();
+      }
+    }
+    if (priorityQueue.size() == 0)
         return make_unique<NullQueryResult>();
       while (priorityQueue.size() != 0) {
         auto top = priorityQueue.top();
@@ -56,7 +95,6 @@ QueryResult::Ptr SelectQuery::execute() {
           oss << "\n";
         priorityQueue.pop();
       }
-    }
     return make_unique<SuccessMsgResult>(oss.str());
   } catch (const TableNameNotFound &e) {
     return make_unique<ErrorMsgResult>(qname, this->targetTable,
--- src/query/data/SumQuery.cpp
+++ src/query/data/SumQuery.cpp
@@ -1,33 +1,75 @@
 #include "SumQuery.h"
 #include "../../db/Database.h"
-
+#include "../../multithreads/MultiThread.hpp"
+/**********************************************/
+/*Define Global Varaibles*/
 constexpr const char *SumQuery::qname;
+static unsigned int total_thread;
+static unsigned int subtable_num;
+
+static Table *copy_table;
+static ComplexQuery *copy_this;
+static std::vector<std::string> *copy_operand;
+static std::pair<std::string, bool> result;
+static int *int_arr;
+/**********************************************/
+void Sub_Sum(int id){
+  auto head = copy_table->begin() + (id * (int)subtable_num);
+  auto tail = id == (int)total_thread - 1 ? copy_table->end()
+                                          : head + (int)subtable_num;
+  if (result.second) {
+    for (auto row = head; row != tail; ++row) {
+        if (copy_this->evalCondition(*row)) {
+          for (size_t i(0); i < copy_operand->size(); i++) {
+            int_arr[i] += (*row)[(*copy_operand)[i]];
+          }
+        }
+    }
+  }
+  return;
+}
 
 QueryResult::Ptr SumQuery::execute() {
   using namespace std;
   Database &db = Database::getInstance();
-  auto size = this->operands.size();
-  if (size <= 0)
+  if (this->operands.size() <= 0)
     return make_unique<ErrorMsgResult>(
         qname, this->targetTable.c_str(),
         "Invalid number of operands (? operands)."_f % operands.size());
   // start of try
   try {
     auto &table = db[this->targetTable];
-    auto result = initCondition(table);
-    int *int_arr = new int[size];
-    for (size_t i = 0; i < size; i++)
+    result = initCondition(table);
+    int_arr = new int[(this->operands).size()];
+    total_thread = (unsigned int) worker.Thread_count();
+    for (size_t i(0); i < this->operands.size(); i++) {
       int_arr[i] = 0;
+    }
+    if (total_thread < 2 || table.size() < 16){
     if (result.second) {
       for (auto row = table.begin(); row != table.end(); ++row) {
         if (this->evalCondition(*row)) {
-          for (size_t i = 0; i < size; i++) {
+          for (size_t i(0); i < this->operands.size(); i++) {
             int_arr[i] += (*row)[this->operands[i]];
           }
         }
       }
     }
-    return make_unique<SuccessMsgResult>(int_arr, size);
+    }
+    else{
+      copy_table = &table;
+      copy_this = this;
+      subtable_num = (unsigned int)(table.size()) / total_thread;
+       std::vector<std::future<void>> futures((unsigned long)total_thread);
+      for(int i = 0; i<(int)total_thread - 1; i++){
+        futures[(unsigned long)i] = worker.Submit(Sub_Sum, i);
+      }
+      Sub_Sum((int)total_thread - 1);
+      for (unsigned long i = 0; i<(unsigned long)total_thread - 1;i++){
+        futures[i].get();
+      }
+    }
+    return make_unique<SuccessMsgResult>(int_arr, this->operands.size());
   } catch (const TableNameNotFound &e) {
     return make_unique<ErrorMsgResult>(qname, this->targetTable,
                                        "No such table."s);
